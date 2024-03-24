59e2ba090de149ba2e2a728070812a32e9947130
ericmorgan1 <ericmorgan1@gmail.com>
2024-03-24 10:33:44 -0500

    Fix build error

--- src/app/api/reso/test/route.ts
+++ src/app/api/reso/test/route.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest } from "next";
 import ParagonApiClient from "@/lib/ParagonApiClient";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const RESO_BASE_URL = process.env.RESO_BASE_URL ?? "";
 const RESO_TOKEN_URL = process.env.RESO_TOKEN_URL ?? "";
@@ -11,7 +11,7 @@ const RESO_CLIENT_SECRET = process.env.RESO_CLIENT_SECRET ?? "";
 //     query: { searchTerm?: string; }
 // }
 
-export async function GET(request: NextApiRequest) {
+export async function GET(request: Request | NextRequest) {
     // TODO: Should be able to use request.query, but it's undefined...
     const url = new URL(request.url ?? "");
     const searchParams = url.searchParams;
