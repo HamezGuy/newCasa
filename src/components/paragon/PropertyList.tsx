0a289d5ee1ea9acc814dfb06e67daa699de4678e
Bruce Castro <brucecastrog@gmail.com>
2024-04-07 18:31:31 -0500

    Add styling to Listings page

--- src/components/paragon/PropertyList.tsx
+++ src/components/paragon/PropertyList.tsx
@@ -30,7 +30,7 @@ export default function PropertyList({
 
   return (
     <div
-      className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ${className}`}
+      className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ${className}`}
     >
       {properties.map((property) => (
         <PropertySearchResultCard
